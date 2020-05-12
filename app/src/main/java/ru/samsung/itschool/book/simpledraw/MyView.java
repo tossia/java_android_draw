c83e08e0260c5008aa6d51c0d79fc743ce43bed2
Antonina Che <tossia2000@yahoo.fr>
2020-05-13 00:35:54 +0200

    many balls

--- app/src/main/java/ru/samsung/itschool/book/simpledraw/MyView.java
+++ app/src/main/java/ru/samsung/itschool/book/simpledraw/MyView.java
@@ -6,18 +6,30 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
-import task.Task;
-
 public class MyView extends View {
+    int N = 4; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
 
     public MyView(Context context) {
-        super(context);
-    }
 
+
+        super(context);
+    for (int i = 0; i < N; i++){
+        x[i] = (float)(Math.random() * getWidth());
+        y[i] = (float)(Math.random() * getHeight());
+        vx[i] = (float)(Math.random() * 6 - 3);
+        vy[i] = (float)(Math.random() * 6 - 3);
+    }
+}
+    //float x = 0;
+  //  long lastTime = System.currentTimeMillis();
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        int y = 0;
+Paint paint = new Paint();
+/*        int y = 0;
         paint.setStrokeWidth(10);
         paint.setColor(Color.GREEN);
         while (y < canvas.getHeight()) {
@@ -42,6 +54,24 @@ public class MyView extends View {
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.FILL);
         canvas.drawRect(300, 600, 900, 1200, paint);
-
+*/
+   //     canvas.drawCircle(x, 500, 20, paint);
+        // готовим x c учетом прошедшего времени
+        // c момента последней перерисовки
+     //   long nowTime = System.currentTimeMillis();
+  //      x += 0.1f * (nowTime - lastTime);
+        // сохраняем время последней перерисовки
+     //   lastTime = nowTime;
+        // отрисовываем все шарики
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        //запрашиваем перерисовку
+        invalidate();
     }
 }
